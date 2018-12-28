ede46f26be6134aeeb6923634b77d1b07cbe2350
Dominik Toczek <dominiktoczek@interia.pl>
2018-12-28 13:46:00 +0100

    Added end-of-game function

--- script.js
+++ script.js
@@ -32,9 +32,18 @@ function handleOrientation(event) {
 
   ball.style.top  = (maxX*x/180 - 10) + "px";
   ball.style.left = (maxY*y/180 - 10) + "px";
+
+  if ((parseInt(ball.style.top) >= parseInt(hole.style.top)) && 
+      (parseInt(ball.style.top) <= parseInt(hole.style.top)+30) &&
+      (parseInt(ball.style.left) >= parseInt(hole.style.left)) && 
+      (parseInt(ball.style.left) <= parseInt(hole.style.left)+30)
+     )
+  {
+      window.alert("Game over");
+      randomHole();
+  }
 }
 
 document.getElementById('start').addEventListener('click', function(){
     window.addEventListener('deviceorientation', handleOrientation);
-    let time = new Date();
 })
